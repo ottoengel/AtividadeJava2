c98552d8cf3edf21f42ea92fd291f2ea18cefefa
Jhonathan dos Santos Tomkiv <141834841+jhonazin@users.noreply.github.com>
2024-04-24 20:23:43 -0300

    terc commit

--- src/Tecnico.java
+++ src/Tecnico.java
@@ -1,3 +1,36 @@
 public class Tecnico {
-    
+    private  String nome;
+    private String id;
+
+    public Tecnico(){
+    }
+   
+    public Tecnico(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        
+        return "\nNome: " + nome + "\nId: " + id;
+ 
+    }
+
 }
---
+++ src/Time.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Time {
+    private int id; // Supondo que cada time tenha um ID
+
+    private static List<Time> listaTime = new ArrayList<>();
+
+    public Time(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static void cadastrar(Time time) {
+        listaTime.add(time);
+    }
+
+    public static List<Time> getListaTime() {
+        return listaTime;
+    }
+
+    public static Time buscar(int id) {
+        for (Time temp : listaTime) {
+            if (temp.getId() == id) {
+                return temp;
+            }
+        }
+        return null;
+    }
+}
